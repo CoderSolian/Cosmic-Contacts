e536d0666a798aec0320fcba0607699dcd555bb6
rdiorio <116819759+rdiorio@users.noreply.github.com>
2024-01-23 23:40:25 -0500

    contactMH script created, fixed the login-container div height to cover the entire screen height

---
+++ front-end/scripts/scriptContactManagerHome.js
@@ -0,0 +1,23 @@
+let contacts = [];
+
+function addContact(contact) {
+    contacts.push(contact);
+    contacts.sort((a, b) => a.name.localeCompare(b.name));
+    // add the html part to update the page with the new contact div's made
+}
+
+function deleteContact(index) {
+    contacts.splice(index, 1);
+    contacts.sort((a, b) => a.name.localeCompare(b.name));
+    // add the html part to update the page with the new contact div's deleted
+}
+
+function searchContact(name) {
+    return contacts.filter(contact => contact.name === name);
+}
+
+function displayContacts() {
+    contacts.forEach(contact => {
+        console.log(contact);
+    });
+}
