715dd7e65739bc3ef631bcc605dc047729f23a73
Michael1Norton <100639225+Michael1Norton@users.noreply.github.com>
2024-01-25 17:48:30 -0500

    Adding Login, Register, and Search. All have been tested/working in postman and should be in the droplet

---
+++ Search.php
@@ -0,0 +1,72 @@
+<?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    $inData = getRequestInfo();
+    
+    $searchResults = "";
+    $searchCount = 0;
+
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+    if ($conn->connect_error) 
+    {
+        returnWithError($conn->connect_error);
+    } 
+    else
+    {
+        $searchTerm = "%" . $inData["search"] . "%";
+        
+        // You can adjust the query based on the fields you want to search
+        $stmt = $conn->prepare("SELECT FirstName, LastName, Phone, Email FROM Contacts WHERE FirstName LIKE ?>
+        $stmt->bind_param("ssss", $searchTerm, $searchTerm, $searchTerm, $searchTerm);
+        $stmt->execute();
+        
+        $result = $stmt->get_result();
+        
+        while ($row = $result->fetch_assoc())
+        {
+            if ($searchCount > 0)
+            {
+                $searchResults .= ",";
+            }
+            $searchCount++;
+            $searchResults .= json_encode($row);
+        }
+        
+        if ($searchCount == 0)
+        {
+            returnWithError("No Records Found");
+        }
+        else
+        {
+            returnWithInfo($searchResults);
+        }
+        
+        $stmt->close();
+        $conn->close();
+    }
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson($obj)
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    function returnWithError($err)
+    {
+        $retValue = '{"results":[],"error":"' . $err . '"}';
+        sendResultInfoAsJson($retValue);
+    }
+
+    function returnWithInfo($searchResults)
+    {
+        $retValue = '{"results":[' . $searchResults . '],"error":""}';
+        sendResultInfoAsJson($retValue);
+    }
+?>
