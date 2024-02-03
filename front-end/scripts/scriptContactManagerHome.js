d3c1420428ca816afa7bee9ed304aadfc153ee0f
rdiorio <116819759+rdiorio@users.noreply.github.com>
2024-02-03 13:57:13 -0500

    contact script added to code.js

--- front-end/scripts/scriptContactManagerHome.js
+++ front-end/scripts/scriptContactManagerHome.js
@@ -1,14 +1,14 @@
 const urlBase = "http://54.82.88.73/php/";
 const extension = "php";
 let contacts = [];
-import { userId, username } from "./code";
+// import { userId, username } from "./code";
 
-// Get the user's ID from the local storage
-let userId = localStorage.getItem('userId');
+// // Get the user's ID from the local storage
+// let userId = localStorage.getItem('userId');
 
 // Setting the welcome message
-let intro = document.getElementById("intro");
-intro.textContent = 'Welcome, ' + username + '!';
+// let intro = document.getElementById("intro");
+// intro.textContent = 'Welcome, ' + username + '!';
 
 let addContactbtn = document.getElementById("addContactButton");
 let editContactbtn = document.getElementById("contact-editBtn");
@@ -64,147 +64,147 @@ deleteContactbtn.onclick = function() {
 }
 
 // Get the button that adds a new contact
-var addBtn = document.getElementById("addNewContactButton");
+// var addBtn = document.getElementById("addNewContactButton");
 
 
-addBtn.onclick = function() {
-    var firstName = document.getElementById("firstName").value;
-    var lastName = document.getElementById("lastName").value;
-    var email = document.getElementById("email").value;
-    var phone = document.getElementById("phone").value;
+// addBtn.onclick = function() {
+//     var firstName = document.getElementById("firstName").value;
+//     var lastName = document.getElementById("lastName").value;
+//     var email = document.getElementById("email").value;
+//     var phone = document.getElementById("phone").value;
 
-    // Validate the input
-    if (!firstName || !lastName || !email || !phone) {
-        alert("All fields are required.");
-        return;
-    }
+//     // Validate the input
+//     if (!firstName || !lastName || !email || !phone) {
+//         alert("All fields are required.");
+//         return;
+//     }
 
-    // If the input is valid, add the contact
-    // addContact({ firstName: firstName, lastName: lastName, email: email, phoneNumber: phone });
-    // updateUIwithNewContact({ firstName: firstName, lastName: lastName, email: email, phoneNumber: phone }, contacts.length - 1);
-    // modal.style.display = "none";
+//     // If the input is valid, add the contact
+//     // addContact({ firstName: firstName, lastName: lastName, email: email, phoneNumber: phone });
+//     // updateUIwithNewContact({ firstName: firstName, lastName: lastName, email: email, phoneNumber: phone }, contacts.length - 1);
+//     // modal.style.display = "none";
 
-    let tmp = {
-        firstName: firstname,
-        lastName: lastname,
-        phoneNumber: phonenumber,
-        emailAddress: emailaddress,
-        userId: userId,
-      };
+//     let tmp = {
+//         firstName: firstname,
+//         lastName: lastname,
+//         phoneNumber: phonenumber,
+//         emailAddress: emailaddress,
+//         userId: userId,
+//       };
 
-      let jsonPayload = JSON.stringify(tmp);
+//       let jsonPayload = JSON.stringify(tmp);
 
-      let url = urlBase + "/AddContacts." + extension;
+//       let url = urlBase + "/AddContacts." + extension;
 
-      let xhr = new XMLHttpRequest();
-      xhr.open("POST", url, true);
-      xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-      try {
-        xhr.onreadystatechange = function () {
-          if (this.readyState == 4 && this.status == 200) {
-            console.log("Contact has been added");
-            // Clear input fields in form
-            document.getElementById("addMe").reset();
-            // reload contacts table and switch view to show
-            loadContacts();
-            showTable();
-          }
-        };
-        xhr.send(jsonPayload);
-      } catch (err) {
-        console.log(err.message);
+//       let xhr = new XMLHttpRequest();
+//       xhr.open("POST", url, true);
+//       xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+//       try {
+//         xhr.onreadystatechange = function () {
+//           if (this.readyState == 4 && this.status == 200) {
+//             console.log("Contact has been added");
+//             // Clear input fields in form
+//             document.getElementById("addMe").reset();
+//             // reload contacts table and switch view to show
+//             loadContacts();
+//             showTable();
+//           }
+//         };
+//         xhr.send(jsonPayload);
+//       } catch (err) {
+//         console.log(err.message);
 
-      }
-}
+//       }
+// }
 
-function addContact(contact) {
-    contacts.push(contact);
-    contacts.sort((a, b) => a.name.localeCompare(b.name));
-    // add the html part to update the page with the new contact div's made
-}
+// function addContact(contact) {
+//     contacts.push(contact);
+//     contacts.sort((a, b) => a.name.localeCompare(b.name));
+//     // add the html part to update the page with the new contact div's made
+// }
 
-function updateUIwithNewContact(contact, index) {
-    // Get the existing elements
-    const lastNameElement = document.getElementById(`contact-lastName-${index}`);
-    const firstNameElement = document.getElementById(`contact-firstName-${index}`);
-    const phoneElement = document.getElementById(`contact-phone-${index}`);
-    const emailElement = document.getElementById(`contact-email-${index}`);
-    // Update the content of the elements
-    if (firstNameElement) {
-        firstNameElement.textContent = contact.firstName;
-    }
+// function updateUIwithNewContact(contact, index) {
+//     // Get the existing elements
+//     const lastNameElement = document.getElementById(`contact-lastName-${index}`);
+//     const firstNameElement = document.getElementById(`contact-firstName-${index}`);
+//     const phoneElement = document.getElementById(`contact-phone-${index}`);
+//     const emailElement = document.getElementById(`contact-email-${index}`);
+//     // Update the content of the elements
+//     if (firstNameElement) {
+//         firstNameElement.textContent = contact.firstName;
+//     }
 
-    if (lastNameElement) {
-        nameElement.textContent = contact.name;
-    }
+//     if (lastNameElement) {
+//         nameElement.textContent = contact.name;
+//     }
 
-    if (phoneElement) {
-        phoneElement.textContent = contact.phoneNumber;
-    }
+//     if (phoneElement) {
+//         phoneElement.textContent = contact.phoneNumber;
+//     }
 
-    if (emailElement) {
-        emailElement.textContent = contact.email;
-    }
-}
+//     if (emailElement) {
+//         emailElement.textContent = contact.email;
+//     }
+// }
 
-function fetchAndAddContact(url) {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            // Assuming data is an array of contact objects
-            data.forEach(contact => {
-                addContact(contact);
-                // Assuming you have a function to update the UI
-                updateUIWithNewContact(contact);
-            });
-        })
-        .catch(error => console.error('Error:', error));
-}
-
-
-function deleteContact(index) {
-    // Remove the contact from the array
-    contacts.splice(index, 1);
-     // Sort the contacts
-     contacts.sort((a, b) => a.name.localeCompare(b.name));
-      // Remove the corresponding HTML element
-      const contactElement = document.getElementById(`contact-${index}`);
-      if (contactElement) {
-          contactElement.parentNode.removeChild(contactElement);
-      }
-      // Update the IDs of the remaining contact elements
-    for (let i = index; i < contacts.length; i++) {
-        const contactElement = document.getElementById(`contact-${i + 1}`);
-        if (contactElement) {
-            contactElement.id = `contact-${i}`;
-        }
-    }
-}
+// function fetchAndAddContact(url) {
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(data => {
+//             // Assuming data is an array of contact objects
+//             data.forEach(contact => {
+//                 addContact(contact);
+//                 // Assuming you have a function to update the UI
+//                 updateUIWithNewContact(contact);
+//             });
+//         })
+//         .catch(error => console.error('Error:', error));
+// }
 
 
-function searchContact(name) {
-    return contacts.filter(contact => contact.name === name);
-}
+// function deleteContact(index) {
+//     // Remove the contact from the array
+//     contacts.splice(index, 1);
+//      // Sort the contacts
+//      contacts.sort((a, b) => a.name.localeCompare(b.name));
+//       // Remove the corresponding HTML element
+//       const contactElement = document.getElementById(`contact-${index}`);
+//       if (contactElement) {
+//           contactElement.parentNode.removeChild(contactElement);
+//       }
+//       // Update the IDs of the remaining contact elements
+//     for (let i = index; i < contacts.length; i++) {
+//         const contactElement = document.getElementById(`contact-${i + 1}`);
+//         if (contactElement) {
+//             contactElement.id = `contact-${i}`;
+//         }
+//     }
+// }
 
-function displayContacts() {
-    contacts.forEach(contact => {
-        console.log(contact);
-    });
-}
 
-function logout() {
-    // delete the token from the local storage
-    // redirect to login page
-  }
+// function searchContact(name) {
+//     return contacts.filter(contact => contact.name === name);
+// }
 
-  let hardcodedContacts = [
-    { name: 'Rossella Diorio', email: 'hello@example.com', phoneNumber: '123-456-7890' },
-    { name: 'Crazy Jane', email: 'jane@example.com', phoneNumber: '234-567-8901' },
-    // Add more contacts as needed
+// function displayContacts() {
+//     contacts.forEach(contact => {
+//         console.log(contact);
+//     });
+// }
 
-  ];
-  for (let i = 0; i < hardcodedContacts.length; i++) {
-    // Add the contact
-    addContact(hardcodedContacts[i]);
-    updateUIwithNewContact(hardcodedContacts[i], i);
-  }
+// function logout() {
+//     // delete the token from the local storage
+//     // redirect to login page
+//   }
+
+//   let hardcodedContacts = [
+//     { name: 'Rossella Diorio', email: 'hello@example.com', phoneNumber: '123-456-7890' },
+//     { name: 'Crazy Jane', email: 'jane@example.com', phoneNumber: '234-567-8901' },
+//     // Add more contacts as needed
+
+//   ];
+//   for (let i = 0; i < hardcodedContacts.length; i++) {
+//     // Add the contact
+//     addContact(hardcodedContacts[i]);
+//     updateUIwithNewContact(hardcodedContacts[i], i);
+//   }
