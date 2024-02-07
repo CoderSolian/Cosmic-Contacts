cc9674d683222464dc2f9ee9042f169e8d2687c3
nickaking02@gmail.com <nickaking02@gmail.com>
2024-02-07 13:59:39 -0500

    used http

--- front-end/index.js
+++ front-end/index.js
@@ -1,5 +1,5 @@
-const https = require('https');
-// const http = require('http');
+// const https = require('https');
+const http = require('http');
 
 const fs = require('fs');
 const path = require('path');
@@ -9,8 +9,8 @@ const options = {
   cert: fs.readFileSync(path.join(__dirname, 'cert.pem'))
 };
 // Create a simple HTTP server
-const server = https.createServer(options, (req, res) => {
-// const server = http.createServer( (req, res) => {
+// const server = https.createServer(options, (req, res) => {
+const server = http.createServer( (req, res) => {
 
   let filePath = '.' + req.url;
 
